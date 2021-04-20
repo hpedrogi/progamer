75501486018dac664c125fc681c449f38cbb837d
hpedrogi <81660609+hpedrogi@users.noreply.github.com>
2021-04-19 22:12:40 -0300

    Add files via upload

---
+++ progamer/src/main/java/br/com/fiap/dao/SetupDAO.java
@@ -0,0 +1,36 @@
+package br.com.fiap.dao;
+
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import br.com.fiap.model.Setup;
+import br.com.fiap.util.EntityManagerFacade;
+
+public class SetupDAO {
+	
+	private EntityManager manager = EntityManagerFacade.get();
+
+	public void save(Setup setup) {
+		
+		
+		
+		manager.getTransaction().begin();
+		manager.persist(setup);
+		manager.getTransaction().commit();
+		
+		manager.close();
+		
+	}
+
+	public List<Setup> getALL() {
+		
+		String jpql = "SELECT s from Setup s";
+		TypedQuery<Setup> createQuery = manager.createQuery(jpql, Setup.class);
+		return createQuery.getResultList();
+		
+	}
+
+}
